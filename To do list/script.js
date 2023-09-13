2c1808d809349e10e9c541c4368d1c662da3c47c
Divyansh Garg <120127517+Divyanshgarg-04@users.noreply.github.com>
2023-09-13 12:29:09 +0530

    Add files via upload

---
+++ To do list/script.js
@@ -0,0 +1,28 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        window.alert("Kindly Enter Task Name!!!!")
+    }
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+        
+            <div class="task">
+            <input type="checkbox" id="task1" name="checked" value="value1">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                
+                <button class="delete">
+                    <i class="far fa-trash-alt"><strong> X </strong></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
